d0d1212044048def922369617400053204a26219
Zafar9708 <aseem@kloudrac.com>
2025-08-09 17:06:06 +0530

    seo fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://growthwayz.com',
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public', // Ensure sitemap is generated in public folder
 };
