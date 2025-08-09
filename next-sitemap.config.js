5f4b99dab71602000a224282579d5bc90c673717
Zafar9708 <aseem@kloudrac.com>
2025-08-09 17:17:02 +0530

    seo fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,8 @@ module.exports = {
   siteUrl: 'https://growthwayz.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  outDir: './public', // Ensure sitemap is generated in public folder
+  outDir: './public',
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'), // ensures homepage is added
+  ],
 };
